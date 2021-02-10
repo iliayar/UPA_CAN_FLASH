29c1ce936eeac122137f77faaf8a460c688b48d6
Ilya <iliayar3@gmail.com>
2021-02-10 11:20:20 +0300

    DataType Builders

--- libcan/service_all.h
+++ libcan/service_all.h
@@ -3,17 +3,17 @@
 #include <vector>
 
 #include "bytes.h"
-#include "service.h"
 #include "map.h"
+#include "service.h"
 
 namespace Can {
 
-#define SERVICE_BEGIN 
+#define SERVICE_BEGIN
 
 #define SUBFUNCION_ENUM_ELEM(type, value) type = value,
 #define SUBFUNCTIONS(...)                            \
     enum class CONCAT(SERVICE, _SubfunctionType) {   \
-	MAP_TUPLE(SUBFUNCION_ENUM_ELEM, __VA_ARGS__) \
+        MAP_TUPLE(SUBFUNCION_ENUM_ELEM, __VA_ARGS__) \
     };
 #define TYPE_INT(...) int
 #define TYPE_VEC(...) std::vector<uint8_t>
@@ -21,16 +21,38 @@ namespace Can {
 #define DATATYPE_FIELD_GETTER(type, name, ...) \
     TYPE_##type(__VA_ARGS__) get_##name() { return m_##name; }
 #define DATATYPE_FIELD(type, name, ...) TYPE_##type(__VA_ARGS__) m_##name;
+#define DATATYPE_FIELD_SETTER(type, name, ...)            \
+    constexpr auto name(TYPE_##type(__VA_ARGS__) value) { \
+        m_##name = value;                                 \
+        return this;                                      \
+    }
 #define DATATYPE_CTR_FIELD(type, name, ...) TYPE_##type(__VA_ARGS__) name
 #define DATATYPE_CTR_FIELD_INIT(_, name, ...) m_##name(name)
+#define DATATYPE_CTR_FIELD_ARG(_, name, ...) m_##name
 #define DATATYPE(name, ...)                                           \
+    class name##_Builder;                                             \
     class name {                                                      \
     public:                                                           \
-        static std::shared_ptr<name> parse(Util::Reader&);                             \
+        static std::shared_ptr<name> parse(Util::Reader&);            \
         name(MAP_TUPLE_LIST(DATATYPE_CTR_FIELD, __VA_ARGS__))         \
             : MAP_TUPLE_LIST(DATATYPE_CTR_FIELD_INIT, __VA_ARGS__) {} \
         MAP_TUPLE(DATATYPE_FIELD_GETTER, __VA_ARGS__)                 \
         std::vector<uint8_t> dump();                                  \
+        static std::unique_ptr<name##_Builder> build() {              \
+            return std::make_unique<name##_Builder>();                \
+        }                                                             \
+                                                                      \
+    private:                                                          \
+        friend class name##_Builder;                                  \
+        MAP_TUPLE(DATATYPE_FIELD, __VA_ARGS__)                        \
+    };                                                                \
+    class name##_Builder {                                            \
+    public:                                                           \
+        std::shared_ptr<name> build() {                               \
+            return std::make_shared<name>(                            \
+                MAP_TUPLE_LIST(DATATYPE_CTR_FIELD_ARG, __VA_ARGS__)); \
+        }                                                             \
+        MAP_TUPLE(DATATYPE_FIELD_SETTER, __VA_ARGS__)                 \
                                                                       \
     private:                                                          \
         MAP_TUPLE(DATATYPE_FIELD, __VA_ARGS__)                        \
@@ -41,9 +63,11 @@ namespace Can {
 #undef SUBFUNCTIONS
 #undef SUBFUNCION_ENUM_ELEM
 #undef DATATYPE_FIELD_GETTER
+#undef DATATYPE_FIELD_SETTER
 #undef DATATYPE_FIELD
 #undef DATATYPE_CTR_FIELD
 #undef DATATYPE_CTR_FIELD_INIT
+#undef DATATYPE_CTR_FIELD_ARG
 #undef DATATYPE
 #undef TYPE_INT
 #undef TYPE_VEC
@@ -52,7 +76,7 @@ namespace Can {
 #undef SERVICE_BEGIN
 #define DATATYPE(...)
 #define SUBFUNCTIONS(...)
-    
+
 // --------------- Service Request --------------------
 #define SERVICE_BEGIN SERVICE = REQUEST_ID,
 enum class ServiceRequestType {
@@ -62,12 +86,13 @@ enum class ServiceRequestType {
 
 #define FIELD_INT(n) uint##n##_t
 #define FIELD_RAW(a) a
-#define FIELD_DATA(a) a*
+#define FIELD_DATA(a) std::shared_ptr<a>
 #define FIELD_VEC() std::vector<uint8_t>
 
 #define DESTRUCT_INT(...)
 #define DESTRUCT_RAW(...)
-#define DESTRUCT_DATA(name, ...) delete m_##name;
+// #define DESTRUCT_DATA(name, ...) delete m_##name;
+#define DESTRUCT_DATA(name, ...)
 #define DESTRUCT_VEC(...)
 #define DESTRUCT(type, name, ...) DESTRUCT_##type(name, __VA_ARGS__)
 
@@ -144,7 +169,7 @@ enum class ServiceRequestType {
 #define SERVICE_BEGIN SERVICE = RESPONSE_ID,
 enum class ServiceResponseType {
 #include "services/services.h"
-	Negative = 0x7f
+    Negative = 0x7f
 };
 #undef SERVICE_BEGIN
 
@@ -154,18 +179,21 @@ enum class ServiceResponseType {
 #define SERVICE_ARG(type, name, ...) FIELD_##type(__VA_ARGS__) name
 #define SERVICE_ARG_INIT(_, name, ...) m_##name(name)
 #define SERVICE_FIELD(type, name, ...) FIELD_##type(__VA_ARGS__) m_##name;
-#define SERVICE_GETTER(type, name, ...) FIELD_##type(__VA_ARGS__) get_##name() { return m_##name; };
-#define SERVICE_BEGIN							\
-	class CONCAT(ServiceResponse_, SERVICE) : public ServiceResponse { \
-public: \
-CONCAT(ServiceResponse_, SERVICE)(MAP_TUPLE_LIST(SERVICE_ARG, RESPONSE_FIELDS)) : MAP_TUPLE_LIST(SERVICE_ARG_INIT, RESPONSE_FIELDS) {} \
-    ServiceResponseType get_type() { \
-	return ServiceResponseType::SERVICE; \
-    } \
-    MAP_TUPLE(SERVICE_GETTER, RESPONSE_FIELDS) \
-private: \
-    MAP_TUPLE(SERVICE_FIELD, RESPONSE_FIELDS) \
-};
+#define SERVICE_GETTER(type, name, ...) \
+    FIELD_##type(__VA_ARGS__) get_##name() { return m_##name; };
+#define SERVICE_BEGIN                                                  \
+    class CONCAT(ServiceResponse_, SERVICE) : public ServiceResponse { \
+    public:                                                            \
+        CONCAT(ServiceResponse_, SERVICE)                              \
+        (MAP_TUPLE_LIST(SERVICE_ARG, RESPONSE_FIELDS))                 \
+            : MAP_TUPLE_LIST(SERVICE_ARG_INIT, RESPONSE_FIELDS) {}     \
+        ServiceResponseType get_type() {                               \
+            return ServiceResponseType::SERVICE;                       \
+        }                                                              \
+        MAP_TUPLE(SERVICE_GETTER, RESPONSE_FIELDS)                     \
+    private:                                                           \
+        MAP_TUPLE(SERVICE_FIELD, RESPONSE_FIELDS)                      \
+    };
 #include "services/services.h"
 #undef SERVICE_ARG
 #undef SERVICE_ARG_INIT
@@ -184,7 +212,7 @@ private: \
 class ServiceResponse_Negative : public ServiceResponse {
 public:
     ServiceResponse_Negative(ServiceRequestType service, uint8_t code)
-	: m_service(service), m_code(code) {}
+        : m_service(service), m_code(code) {}
 
     ServiceResponseType get_type() { return ServiceResponseType::Negative; }
 
