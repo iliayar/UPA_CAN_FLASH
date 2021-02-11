a74f4733937f41c1c401e0cf5840d0c91a484f34
Ilya <iliayar3@gmail.com>
2021-02-11 17:58:36 +0300

    flasg moved to main, new GUI logger

--- libcan/logger.h
+++ libcan/logger.h
@@ -1,13 +1,27 @@
 #pragma once
 
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "frame.h"
 #include "service.h"
 
-#include <string>
-#include <memory>
-
 namespace Can {
 
+    enum class LogLevel {
+        FrameReceived,
+        FrameTransmitted,
+        SericesReceived,
+        ServiceTransmitted,
+        Error,
+        Info,
+        Warning
+    };
+    
 class Logger {
 public:
     virtual void received_frame(std::shared_ptr<Frame>) = 0;
@@ -17,6 +31,33 @@ public:
     virtual void error(std::string) = 0;
     virtual void info(std::string) = 0;
     virtual void warning(std::string) = 0;
+
+    template<typename T>
+    void log(LogLevel level, T data) {
+        switch(level) {
+        case LogLevel::Error:
+            error(data);
+            break;
+        case LogLevel::Info:
+            info(data);
+            break;
+        case LogLevel::Warning:
+            warning(data);
+            break;
+        case LogLevel::FrameReceived:
+            received_frame(data);
+            break;
+        case LogLevel::FrameTransmitted:
+            transmitted_frame(data);
+            break;
+        case LogLevel::SericesReceived:
+            received_service_response(data);
+            break;
+        case LogLevel::ServiceTransmitted:
+            transmitted_service_request(data);
+            break;
+        }
+    }
 };
 
 class NoLogger : public Logger {
@@ -30,4 +71,59 @@ public:
     void warning(std::string _) {}
 };
 
-}
+class FramesStdLogger : public Logger {
+public:
+    FramesStdLogger() : m_start(std::chrono::high_resolution_clock::now()) {}
+
+    void transmitted_frame(std::shared_ptr<Frame> frame) override {
+        std::cout << "Tester request: ";
+        print_frame(frame);
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+
+    void received_frame(std::shared_ptr<Frame> frame) override {
+        std::cout << "ECU response:   ";
+        print_frame(frame);
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+
+    void received_service_response(ServiceResponse* r) override {
+        // std::cout << "Received response " << (int)r->get_type() << std::endl;
+    }
+
+    void transmitted_service_request(ServiceRequest* r) override {
+        // std::cout << "Transmited request " << (int)r->get_type() <<
+        // std::endl;
+    }
+
+    void error(std::string s) override {
+        std::cout << "ERROR: " << s << std::endl;
+    }
+
+    void info(std::string s) override {
+        std::cout << "INFO: " << s << std::endl;
+    }
+    void warning(std::string s) override {
+        std::cout << "WARNING: " << s << std::endl;
+    }
+
+private:
+    void print_frame(std::shared_ptr<Frame> frame) {
+        std::vector<uint8_t> payload = frame->dump();
+        std::cout << std::setfill(' ') << std::dec << std::setw(4)
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(
+                         std::chrono::high_resolution_clock::now() - m_start)
+                         .count()
+                  << " | ";
+        for (uint8_t b : payload) {
+            std::cout << std::setw(2) << std::setfill('0') << std::hex
+                      << (int)b;
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::chrono::high_resolution_clock::time_point m_start;
+};
+
+}  // namespace Can
--- main/can.cpp
+++ main/can.cpp
@@ -5,30 +5,13 @@
 #include <memory>
 #include <QDateTime>
 
-void QFlashTask::task() {
-    Can::ServiceResponse* response;
-    response =
-        call(Can::ServiceRequest_DiagnosticSessionControl::build()
-                 ->subfunction(Can::DiagnosticSessionControl_SubfunctionType::
-                                   extendDiagnosticSession)
-                 ->build());
-}
-
-QLogger::QLogger(QObject* parent, QTextEdit* frame_log, QTextEdit* message_log)
+QLoggerWorker::QLoggerWorker(QObject* parent, QTextEdit* frame_log, QTextEdit* message_log)
     : QObject(parent), m_frame_log(frame_log), m_message_log(message_log), m_timer(), m_mutex() {
 
     m_timer.start();
-    
-    // received_frame(std::make_shared<Can::Frame_SingleFrame>(3, std::vector<uint8_t>({0x01, 0x02, 0x03})));
-    // transmitted_frame(std::make_shared<Can::Frame_SingleFrame>(3, std::vector<uint8_t>({0x01, 0xa2, 0x03})));
-    // received_frame(std::make_shared<Can::Frame_SingleFrame>(3, std::vector<uint8_t>({0x01, 0x02, 0x03})));
-    // transmitted_frame(std::make_shared<Can::Frame_SingleFrame>(3, std::vector<uint8_t>({0x01, 0x02, 0x03})));
-    // error("Test error");
-    // info("Test info");
-    // warning("Test warning");
 }
 
-void QLogger::received_frame(std::shared_ptr<Can::Frame> frame)
+void QLoggerWorker::received_frame(std::shared_ptr<Can::Frame> frame)
 {
     std::unique_lock<std::mutex> lock(m_mutex);
     m_frame_log->setTextColor(QColor("blue"));
@@ -39,7 +22,7 @@ void QLogger::received_frame(std::shared_ptr<Can::Frame> frame)
     m_frame_log->setTextColor(QColor("black"));
 }
 
-QString QLogger::vec_to_qstr(std::vector<uint8_t> vec) {
+QString QLoggerWorker::vec_to_qstr(std::vector<uint8_t> vec) {
     QString str;
     for(uint8_t b : vec) {
         str.append(QString("%1").arg(b, 2, 16, QLatin1Char('0')));
@@ -53,7 +36,7 @@ QString QLogger::vec_to_qstr(std::vector<uint8_t> vec) {
     return str;
 }
 
-void QLogger::transmitted_frame(std::shared_ptr<Can::Frame> frame)
+void QLoggerWorker::transmitted_frame(std::shared_ptr<Can::Frame> frame)
 {
     std::unique_lock<std::mutex> lock(m_mutex);
     std::vector<uint8_t> payload = frame->dump();
@@ -61,30 +44,30 @@ void QLogger::transmitted_frame(std::shared_ptr<Can::Frame> frame)
     payload_str.append(vec_to_qstr(payload));
     m_frame_log->append(payload_str);
 }
-void QLogger::received_service_response(Can::ServiceResponse*)
+void QLoggerWorker::received_service_response(Can::ServiceResponse*)
 {}
-void QLogger::transmitted_service_request(Can::ServiceRequest*)
+void QLoggerWorker::transmitted_service_request(Can::ServiceRequest*)
 {}
 
 QString get_date_str() {
     return "[" + QDateTime::currentDateTime().toString("yyyy/MM/dd hh:mm:ss,zzz") + "]";
 }
 
-void QLogger::info(std::string message)
+void QLoggerWorker::info(std::string message)
 {
     std::unique_lock<std::mutex> lock(m_mutex);
     m_message_log->setTextColor(QColor("gray"));
     m_message_log->append(get_date_str() + "    INFO: " + QString::fromStdString(message));
     m_message_log->setTextColor(QColor("black"));
 }
-void QLogger::error(std::string message)
+void QLoggerWorker::error(std::string message)
 {
     std::unique_lock<std::mutex> lock(m_mutex);
     m_message_log->setTextColor(QColor("red"));
     m_message_log->append(get_date_str() + "   ERROR: " + QString::fromStdString(message));
     m_message_log->setTextColor(QColor("black"));
 }
-void QLogger::warning(std::string message)
+void QLoggerWorker::warning(std::string message)
 {
     std::unique_lock<std::mutex> lock(m_mutex);
     m_message_log->setTextColor(QColor("orange"));
